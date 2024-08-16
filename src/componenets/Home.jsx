2b480a679b36b5e4787319ddd517fdbd8cd7990f
Muhammad Abdullah <iamagoodboi4@gmail.com>
2024-08-16 16:03:28 +0500

    made it subtle

--- src/componenets/Home.jsx
+++ src/componenets/Home.jsx
@@ -1,49 +1,30 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
-import video from "../images/video.mp4";
-import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa'; // Importing icons
-
+import { AuroraBackground } from "./ui/aurora-background";
+import { SiReact, SiExpress, SiMongodb, SiNodedotjs } from "react-icons/si";
+import "./Home.css";
 const Home = () => {
-  const videoRef = useRef(null);
-  const [isMuted, setIsMuted] = useState(true);
-
-  const toggleSound = () => {
-    if (videoRef.current) {
-      videoRef.current.muted = !isMuted;
-      setIsMuted(!isMuted);
-    }
-  };
-
   return (
     <div className="relative h-screen flex flex-col bg-dark">
       <Navbar />
-      <div className="absolute inset-0 top-[100px]"> {/* Adjust the top value if needed */}
-        <video
-          ref={videoRef}
-          src={video} // Replace with your video URL
-          autoPlay
-          loop
-          className="w-full h-full object-cover"
-          muted={isMuted} // Initial muted state
-        />
-        <div className="absolute inset-0 bg-black opacity-80" /> {/* Dark shade overlay */}
-      </div>
-      <div className="relative flex flex-1 items-center justify-center z-10">
-        <div className="text-center animate-fade-in">
-          <h1 className="text-[80px] sm:text-[100px] md:text-[120px] lg:text-[160px] xl:text-[220px] font-BebasNeue font-extrabold text-[#edcd4d] leading-none">
-            Blackburn
-          </h1>
-          <p className="text-white font-BebasNeue text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-            Interlinked
-          </p>
+      <AuroraBackground>
+        <div className="relative flex flex-1 items-center justify-center z-10">
+          <div className="text-center ">
+            <h1 className="text-[80px] sm:text-[100px] md:text-[120px] lg:text-[160px] xl:text-[220px] font-BebasNeue font-extrabold text-[#edcd4d] leading-none animate-glowing">
+              Blackburn
+            </h1>
+            <p className="text-white font-BebasNeue text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+              Interlinked
+            </p>
+          </div>
         </div>
-      </div>
-      <button
-        onClick={toggleSound}
-        className="absolute z-50 bottom-4 right-4 p-3 bg-gray-800 text-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-700 transition duration-300"
-      >
-        {isMuted ? <FaVolumeMute size={24} /> : <FaVolumeUp size={24} />}
-      </button>
+        <div className="flex justify-center items-center text-center gap-4 text-white mt-4 text-2xl">
+          <SiMongodb />
+          <SiExpress />
+          <SiReact />
+          <SiNodedotjs />
+        </div>
+      </AuroraBackground>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,15 +8,42 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        "dark": "#0F0F0F",
-        "primary": "#edcd4d"
+      animation: {
+        aurora: "aurora 60s linear infinite",
       },
-      fontFamily:{
-      "Quicksand": ["Quicksand"],
-      "BebasNeue": ["Bebas Neue"]
-      }
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+      colors: {
+        dark: "#0F0F0F",
+        primary: "#edcd4d",
+      },
+      fontFamily: {
+        Quicksand: ["Quicksand"],
+        BebasNeue: ["Bebas Neue"],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors, // Registering the custom plugin
+  ],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
